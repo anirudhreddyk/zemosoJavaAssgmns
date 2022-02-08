2711790e3f819b9c1d258e51c728c7b15c6816f6
anirudhreddyk <97898316+anirudhreddyk@users.noreply.github.com>
2022-02-08 10:16:02 +0530

    assignment 7

---
+++ assgn7/InnerClass.java
@@ -0,0 +1,27 @@
+package assgn7;
+
+
+class FirstOuter {
+
+    class FirstInner {
+        public FirstInner(String name) {
+            System.out.println(name);
+        }
+    }
+}
+
+class SecondOuter {
+    class SecondInner extends FirstOuter.FirstInner {
+        public SecondInner(FirstOuter firstouter, String name) {
+            firstouter.super(name);
+        }
+    }
+}
+
+class InnerClass {
+    public static void main(String args[]) {
+        FirstOuter firstouter = new FirstOuter();
+        SecondOuter so = new SecondOuter();
+        SecondOuter.SecondInner obj = so.new SecondInner(firstouter, "Anirudh");
+    }
+}
